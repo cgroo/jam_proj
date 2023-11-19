72ad0d43cdbbec0e9d377054a54515a7f3c5c9f6
cgroo <glennangelo78@gmail.com>
2023-11-19 14:36:51 +1300

    Added scheduled timer event

--- vpet_java/command_line/ScheduledEvents.java
+++ vpet_java/command_line/ScheduledEvents.java
@@ -1,5 +1,27 @@
 package command_line;
 
-public class ScheduledEvents {
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class ScheduledEvents {
+    private Pet pet;
+
+    public ScheduledEvents(Pet pet) {
+        this.pet = pet;
+        startScheduling();
+
+    }
+
+    public void startScheduling() {
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new HungerReductionTask(), 0, 10000);
+    }
+
+    private class HungerReductionTask extends TimerTask {
+        @Override
+        public void run() {
+            pet.removeHunger(10);
+            System.out.println("Hunger reduced. Current hunger levl: " + pet.getHungerLevel());
+        }
+    }
 }
