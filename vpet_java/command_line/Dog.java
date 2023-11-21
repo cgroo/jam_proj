84a26321193a271d15fd3bfd65f66feb5fd760fc
cgroo <glennangelo78@gmail.com>
2023-11-21 20:00:04 +1300

    Unfinished: Dog toString method

--- vpet_java/command_line/Dog.java
+++ vpet_java/command_line/Dog.java
@@ -68,13 +68,30 @@ public class Dog extends Pet {
 		}
 	}
 
+	private String buildSpaces(String value, String field) {
+		String spaces = "";
+		for (int i = 0; i < 18 - value.length() - field.length(); i++) {
+			spaces += " ";
+		}
+		return spaces;
+	}
+
 	public String toString() {
-		String fullString = "---------------------------" + '\n';
-		fullString += "| " + petName + "    Age: " + petAge + "|" + '\n';
-		fullString += "| Happiness: " + happinessLevel + "     |" + '\n';
-		fullString += "| Cleanliness: " + cleanLevel + "     |" + '\n';
-		fullString += "| Hunger: " + hungerLevel + "     |" + '\n';
-		fullString += "| Thirst: " + thirstLevel + "     |" + '\n';
+		String spaces = "";
+		String fullString = "------------------------" + '\n';
+		spaces = buildSpaces(petName, "Name");
+		fullString += "| Name: " + spaces + petName + "|" + '\n';
+		spaces = buildSpaces(String.valueOf(petAge), "Age");
+		fullString += "| Age: " + spaces + petAge + "|" + '\n';
+		spaces = buildSpaces(String.valueOf(petAge), "Happiness");
+		fullString += "| Happiness: " + spaces + happinessLevel + "|" + '\n';
+		spaces = buildSpaces(String.valueOf(petAge), "Cleanliness");
+		fullString += "| Cleanliness: " + spaces + cleanLevel + "|" + '\n';
+		spaces = buildSpaces(String.valueOf(petAge), "Hunger");
+		fullString += "| Hunger: " + spaces + hungerLevel + "|" + '\n';
+		spaces = buildSpaces(String.valueOf(petAge), "Thirst");
+		fullString += "| Thirst: " + spaces + thirstLevel + "|" + '\n';
+		fullString += "------------------------" + '\n';
 		return fullString;
 	}
 }
--- vpet_java/command_line/Main.java
+++ vpet_java/command_line/Main.java
@@ -1,10 +1,11 @@
 package command_line;
+
 import java.util.Scanner;
 
 public class Main {
 	private Pet pet;
 	private User user;
-	
+
 	public void startUp() {
 		Scanner input = new Scanner(System.in);
 		System.out.println("What is your name?");
@@ -20,23 +21,16 @@ public class Main {
 		}
 
 	}
-	
+
 	public void petTesting() {
 		if (pet instanceof Dog) {
-			Dog dog = (Dog)pet;
-			dog.showCleanliness(); //should be a lil stinky becuz dog is = 50 
-			dog.addClean(40);
-			dog.showCleanliness(); //should be smelling great now
-			dog.removeClean(80);
-			dog.showCleanliness(); //should be smelling like angelo's fart
-			
+			System.out.println(pet);
 
-		}
-		else {
+		} else {
 			System.out.println("Unsupported Pet Type");
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		Main newProgram = new Main();
 		newProgram.startUp();
