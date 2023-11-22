050717ce8bd4bc95fde1432eacffae74f05c3b83
cgroo <glennangelo78@gmail.com>
2023-11-22 20:40:02 +1300

    Testinig for Creation of Cats and Rats working

--- vpet_java/command_line/Main.java
+++ vpet_java/command_line/Main.java
@@ -10,22 +10,36 @@ public class Main {
 		Scanner input = new Scanner(System.in);
 		System.out.println("What is your name?");
 		String name = input.nextLine();
-		System.out.println("What pet do you want? (Type Dog)");
+		System.out.println("What pet do you want? (Type Dog, Rat, or Cat)");
 		String petType = input.nextLine();
 		System.out.println("What is the name of your new pet?");
 		String petName = input.nextLine();
 		input.close();
+
 		user = new User(name);
 		if (petType.toLowerCase().equals("dog")) {
 			pet = new Dog(petName);
 		}
-
+		if (petType.toLowerCase().equals("cat")) {
+			pet = new Cat(petName);
+		}
+		if (petType.toLowerCase().equals("rat")) {
+			pet = new Rat(petName);
+		}
 	}
 
 	public void petTesting() {
 		if (pet instanceof Dog) {
 			System.out.println(pet);
 			pet.listOfTricks();
+		}
+
+		else if (pet instanceof Cat) {
+			System.out.println(pet);
+		}
+
+		else if (pet instanceof Rat) {
+			System.out.println(pet);
 
 		} else {
 			System.out.println("Unsupported Pet Type");
