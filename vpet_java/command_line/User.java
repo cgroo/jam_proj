32434e3c342166eff002cf760b9c90786f673b1a
cgroo <glennangelo78@gmail.com>
2023-11-22 21:11:19 +1300

    Added money attribute for the player

--- vpet_java/command_line/User.java
+++ vpet_java/command_line/User.java
@@ -2,12 +2,18 @@ package command_line;
 
 public class User {
 	String userName;
+	int userMoney;
 
 	public User(String name) {
 		userName = name;
+		userMoney = 50;
 	}
 
 	public String getName() {
 		return userName;
+	}
+
+	public int getMoney() {
+		return userMoney;
 	}
 }
