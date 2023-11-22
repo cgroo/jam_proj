7a4911640f766c0a29d53e13a71ccd0cd62c4e8c
cgroo <glennangelo78@gmail.com>
2023-11-22 21:11:48 +1300

    Rules of the game now given to user

--- vpet_java/command_line/Main.java
+++ vpet_java/command_line/Main.java
@@ -1,6 +1,7 @@
 package command_line;
 
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 	private Pet pet;
@@ -28,28 +29,61 @@ public class Main {
 		}
 	}
 
-	public void petTesting() {
-		if (pet instanceof Dog) {
-			System.out.println(pet);
-			pet.listOfTricks();
-		}
+	/**
+	 * public void petTesting() {
+	 * if (pet instanceof Dog) {
+	 * System.out.println(pet);
+	 * pet.listOfTricks();
+	 * }
+	 * 
+	 * else if (pet instanceof Cat) {
+	 * System.out.println(pet);
+	 * }
+	 * 
+	 * else if (pet instanceof Rat) {
+	 * System.out.println(pet);
+	 * 
+	 * } else {
+	 * System.out.println("Unsupported Pet Type");
+	 * }
+	 * }
+	 * 
+	 */
 
-		else if (pet instanceof Cat) {
-			System.out.println(pet);
-		}
+	public void gameRules() {
+		System.out.println("Welcome to 'this game' (STC)  ");
+		System.out.println("Here are the rules of the game:");
+		System.out.println(
+				"You will soon be assigned a pet. This pet is real; a living organism, and you musn't let it die");
+		System.out
+				.println("Your pet has many attibutes such as its Happiness, its Cleanliness, its Hunger and Thirst.");
+		System.out.println("It is vital that you keep these attributes up to keep your pet alive");
+		System.out
+				.println("If at any point your pet has 1 attribute at 0, you lose and both you and your pet WILL DIE");
+		System.out.println(
+				"To keep your pet happy you can teach them tricks and play games by typing 'Tricks' and 'Play' respectively");
+		System.out.println("To keep your pet clean you must wipe them down and bath them by typing 'Clean'");
+		System.out.println(
+				"To keep your pet full you must feed them regularly by buying food with your money accumulated by tasks. These menus are accessed via 'Shop' and 'Work'");
+		System.out.println("To keep your pet quenched you must give them water regularly by typing 'Water'");
+		System.out.println("If at any point you need a reminder of the keywords type 'Help");
+		System.out.println("We hope you enjoy the game and remember");
+		System.out.println("Don't let your pet die" + '\n');
+		System.out.println("Keywords: 'Tricks', 'Play', 'Clean', 'Shop', 'Work', 'Water'");
 
-		else if (pet instanceof Rat) {
-			System.out.println(pet);
+	}
 
-		} else {
-			System.out.println("Unsupported Pet Type");
-		}
+	public void mainLogic() {
+
 	}
 
 	public static void main(String[] args) {
 		Main newProgram = new Main();
 		newProgram.initialise();
-		newProgram.petTesting();
+		ScheduledEvents petAttributeDecay = new ScheduledEvents(newProgram.pet);
+		// newProgram.petTesting();
+		newProgram.gameRules();
+		newProgram.mainLogic();
 
 	}
 
