159a49404d7b4561f3b8d3eea8896d4d16afca95
cgroo <glennangelo78@gmail.com>
2023-11-22 20:32:23 +1300

    Finished constructors for Cat and Rat

--- vpet_java/command_line/Cat.java
+++ vpet_java/command_line/Cat.java
@@ -1,50 +1,47 @@
 package command_line;
 
-public class Cat extends Pet{
+public class Cat extends Pet {
+
+	public Cat(String petName) {
+		super(petName);
+	}
+
 	public void showHappiness() {
 		if (this.happinessLevel >= 90) {
 			System.out.println("Meow! :) i'm happy");
-		}
-		else if (this.happinessLevel >= 50) {
+		} else if (this.happinessLevel >= 50) {
 			System.out.println("Meow :l i'm alright");
-		}
-		else {
+		} else {
 			System.out.println("meow :( give me attention");
 		}
 	}
-		
+
 	public void showCleanliness() {
 		if (this.cleanLevel >= 90) {
 			System.out.println("Meow! :) i smell great");
-		}
-		else if (this.cleanLevel >= 50) {
+		} else if (this.cleanLevel >= 50) {
 			System.out.println("Meow :l i'm a lil stinky");
-		}
-		else {
+		} else {
 			System.out.println("meow :( please clean me");
 		}
 	}
-	
+
 	public void showFullness() {
 		if (this.hungerLevel >= 90) {
 			System.out.println("Meow! :) i'm so full");
-		}
-		else if (this.hungerLevel >= 50) {
+		} else if (this.hungerLevel >= 50) {
 			System.out.println("Meow :l stomach's grumbling a bit");
-		}
-		else {
+		} else {
 			System.out.println("meow :( please feed me");
 		}
 	}
-	
+
 	public void showQuench() {
 		if (this.thirstLevel >= 90) {
 			System.out.println("Meow! :) i'm not thirsty");
-		}
-		else if (this.hungerLevel >= 50) {
+		} else if (this.hungerLevel >= 50) {
 			System.out.println("Meow :l i could go for some water");
-		}
-		else {
+		} else {
 			System.out.println("meow :( i'm dying of thirst");
 		}
 	}
--- vpet_java/command_line/Rat.java
+++ vpet_java/command_line/Rat.java
@@ -1,51 +1,47 @@
 package command_line;
 
-public class Rat extends Pet{
-	public Rat() {
+public class Rat extends Pet {
+
+	public Rat(String petName) {
+		super(petName);
+	}
+
 	public void showHappiness() {
 		if (this.happinessLevel >= 90) {
 			System.out.println("Squeak! :) i'm happy");
-		}
-		else if (this.happinessLevel >= 50) {
+		} else if (this.happinessLevel >= 50) {
 			System.out.println("Squeak :l i'm alright");
-		}
-		else {
+		} else {
 			System.out.println("squeak :( give me attention");
 		}
 	}
-		
+
 	public void showCleanliness() {
 		if (this.cleanLevel >= 90) {
 			System.out.println("Squeak! :) i smell great");
-		}
-		else if (this.cleanLevel >= 50) {
+		} else if (this.cleanLevel >= 50) {
 			System.out.println("Squeak :l i'm a lil stinky");
-		}
-		else {
+		} else {
 			System.out.println("squeak :( please clean me");
 		}
 	}
-	
+
 	public void showFullness() {
 		if (this.hungerLevel >= 90) {
 			System.out.println("Squeak! :) i'm so full");
-		}
-		else if (this.hungerLevel >= 50) {
+		} else if (this.hungerLevel >= 50) {
 			System.out.println("Squeak :l stomach's grumbling a bit");
-		}
-		else {
+		} else {
 			System.out.println("squeak :( please feed me");
 		}
 	}
-	
+
 	public void showQuench() {
 		if (this.thirstLevel >= 90) {
 			System.out.println("Squeak! :) i'm not thirsty");
-		}
-		else if (this.hungerLevel >= 50) {
+		} else if (this.hungerLevel >= 50) {
 			System.out.println("Squeak :l i could go for some water");
-		}
-		else {
+		} else {
 			System.out.println("squeak :( i'm dying of thirst");
 		}
 	}
