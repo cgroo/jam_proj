4f88e096b9173cd4744a30d2b51deddaa6199b81
cgroo <glennangelo78@gmail.com>
2023-11-23 20:55:54 +1300

    added getRemainingTime method

--- vpet_java/command_line/EasyTimer.java
+++ vpet_java/command_line/EasyTimer.java
@@ -6,8 +6,14 @@ import java.util.concurrent.TimeUnit;
 
 public class EasyTimer {
     private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+    private long startTime;
+    private long duration;
+    private boolean timerOn;
 
     public void countdown(int seconds, Runnable actionOnCompletion) {
+        this.startTime = System.currentTimeMillis();
+        this.duration = seconds * 1000L;
+        this.timerOn = true;
         executorService.schedule(() -> {
             for (int i = seconds; i > 0; i--) {
                 System.out.println(i);
@@ -19,6 +25,22 @@ public class EasyTimer {
             }
             if (actionOnCompletion != null) {
                 actionOnCompletion.run();
+            }
+        }, 0, TimeUnit.SECONDS);
+    }
+
+    public long getRemainingTime() {
+        return duration - (System.currentTimeMillis() - startTime);
+    }
+
+    public void wait(int seconds) {
+        executorService.schedule(() -> {
+            for (int i = seconds; i > 0; i--) {
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
             }
         }, 0, TimeUnit.SECONDS);
     }
